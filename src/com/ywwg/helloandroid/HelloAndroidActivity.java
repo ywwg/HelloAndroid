9a51be64d46971256a8304a1ebefdda2c63c0f2d
Owen Williams <owen-github@ywwg.com>
2012-05-06 22:37:34 -0400

    Initial import I hope

---
+++ src/com/ywwg/helloandroid/HelloAndroidActivity.java
@@ -0,0 +1,19 @@
+package com.ywwg.helloandroid;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+
+public class HelloAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.my_music);
+        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+    }
+}
+
