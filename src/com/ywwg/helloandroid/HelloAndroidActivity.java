694c82cfadda65601f0f5b391fb4642642587726
Mike MacHenry <dskippy@dskippy-Latitude-D830.(none)>
2012-05-19 13:05:04 -0400

    Hello

--- src/com/ywwg/helloandroid/HelloAndroidActivity.java
+++ src/com/ywwg/helloandroid/HelloAndroidActivity.java
@@ -12,10 +12,14 @@ public class HelloAndroidActivity extends Activity {
         setContentView(R.layout.main);
 
         /*
+         * Owen's mom
+         */
+
+        /*
          * MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.my_music);
          * mediaPlayer.start(); // no need to call prepare(); create() does that
          *for you mediaPlayer.seekTo(10*1000);
-		 */
+         */
 
         Camera mCamera;
         mCamera = Camera.open();
