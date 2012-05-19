228d556f88123c4d8b3ff8b0fc2c1f2e56874463
Owen Williams <owen-github@ywwg.com>
2012-05-19 12:44:36 -0400

    Trying out camera stuff

--- src/com/ywwg/helloandroid/HelloAndroidActivity.java
+++ src/com/ywwg/helloandroid/HelloAndroidActivity.java
@@ -1,26 +1,28 @@
 package com.ywwg.helloandroid;
 
 import android.app.Activity;
-import android.media.MediaPlayer;
+import android.hardware.Camera;
 import android.os.Bundle;
 
-
 public class HelloAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.my_music);
-        mediaPlayer.start(); // no need to call prepare(); create() does that for you
-        mediaPlayer.seekTo(10*1000);
-        
-        /*Camera.Parameters params = mCamera.getParameters();
-        String value;
-        value = Camera.Parameters.FLASH_MODE_TORCH;
-        params.setFlashMode(value);*/
-        
-    }
-}
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
+		/*
+		 * MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.my_music);
+		 * mediaPlayer.start(); // no need to call prepare(); create() does that
+		 * for you mediaPlayer.seekTo(10*1000);
+		 */
+
+		Camera mCamera;
+		mCamera = Camera.open();
+		
+		Camera.Parameters params = mCamera.getParameters(); 
+		String value;
+		value = Camera.Parameters.FLASH_MODE_TORCH;
+		params.setFlashMode(value);
+	}
+}
